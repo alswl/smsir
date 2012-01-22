e2a1dc432d75a1c7406975ff0f326fbb0703f321
alswl <alswlx@gmail.com>
2012-01-22 11:50:11 +0800

    fix bug unicode

--- model/contact.py
+++ model/contact.py
@@ -27,4 +27,4 @@ class Contact(Base):
         # TODO update for first name / last name
         self.name = self.name.replace(' ', '')
         self.name = self.name.replace('!', '')
-        self.name = self.name.replace('！', '')
+        self.name = self.name.replace(u'！', '')
